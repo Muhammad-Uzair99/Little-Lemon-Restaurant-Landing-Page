18d0145451d90452d71c7d8b2ba19673ff4234cd
Muhammad-Uzair99 <uzairmaster97@gmail.com>
2023-06-17 14:36:46 +0500

    Seperated JavaScript form Html and used best practices to write javascript

---
+++ scripts/homePage/navBtn.js
@@ -0,0 +1,17 @@
+(() => {
+  "use strict";
+  document.addEventListener("DOMContentLoaded", () => {
+    const hamburgerBtn = document.querySelector("#hamburger-btn");
+    const crossBtn = document.querySelector("#cross-btn");
+    const navEl = document.querySelector("#nav");
+
+    hamburgerBtn.addEventListener("click", toggleBtn);
+    crossBtn.addEventListener("click", toggleBtn);
+
+    function toggleBtn() {
+      hamburgerBtn.classList.toggle("hidden");
+      crossBtn.classList.toggle("hidden");
+      navEl.classList.toggle("hidden");
+    }
+  });
+})();
